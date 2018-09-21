3918e5a623913cdd577056a242317a0484be6d8e
Suyash Srivastava <suyashsr644@gmail.com>
2018-09-21 15:36:06 +0530

    start api

--- pr/profile_project/profile_api/views.py
+++ pr/profile_project/profile_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloApiView(APIView):
+    def get(self,request, format=None):
+        """ Return a list of APIView fetures """
+        an_api = ['suyash',
+        'nitesg',
+        'okself',
+        'nothing']
+        return Response({"message":'Hello!',"an_api":an_api})
